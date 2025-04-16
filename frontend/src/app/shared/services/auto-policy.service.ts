5c4ddc661ba2b8e21f7df37e07728fcbc6f8bc56
GregDawe <gregid@proton.me>
2025-04-16 12:51:54 -0230

    auto policy frontend data test

--- frontend/src/app/shared/services/auto-policy.service.ts
+++ frontend/src/app/shared/services/auto-policy.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class AutoPolicyService {
 
+
   constructor(private http: HttpClient) {}
 
   getAutoQuote(): Observable<IAutoPolicy[]> {
@@ -18,8 +19,24 @@ export class AutoPolicyService {
   //   this.http.post<any>('URL GOES HERE');
   // }
 
-  postAutoQuote(data: IAutoPolicy): Observable<object> {
-    return this.http.post<IAutoPolicy>('URL GOES HERE', {data}); 
+  postAutoQuote(data: any): Observable<object> {
+
+    let body = {
+      "customerId": 1,
+      "startDate": "2025-04-16",
+      "endDate": "2026-04-16",
+      "basePremium": 750,
+      "premium": 0.0,
+      "status": "PENDING",
+      "vehicle": {
+        "vehicleMake": data.vehicleMake,
+        "vehicleModel": data.vehicleModel,
+        "vehicleYear": data.vehicleYear,
+        "vehicleAccidents": data.vehicleYear
+      }
+    }
+
+    return this.http.post<IAutoPolicy>('http//:localhost:8080/v1/autopolicies', {data}); 
   }
 
   // postMockQuote(data: any): any {
