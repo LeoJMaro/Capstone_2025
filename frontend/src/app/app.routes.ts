b6039eeabf3aa1dc90014e06593b6368ea96c07c
Joel <leojmaro@gmail.com>
2025-04-17 03:53:24 -0230

    fixed gregs work for main

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,8 +7,6 @@ import { LoginComponent } from './components/login/login.component';
 import { HomePolicyComponent } from './components/services/home-policy/home-policy.component';
 import { AutoPolicyComponent } from './components/services/auto-policy/auto-policy.component';
 import { ManagePoliciesComponent } from './components/services/manage-policies/manage-policies.component';
-import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
-import { UserManagementComponent } from './components/admin/user-management/user-management.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -17,27 +15,17 @@ export const routes: Routes = [
   // { path: 'services', component: ServicesComponent },
   // { path: 'home-policy', component: HomePolicyComponent },
 
-  { path: 'services', component: ServicesComponent,
+  { path: 'services', component: ServicesComponent, 
     children: [
       { path: 'home-policy', component: HomePolicyComponent },
       { path: 'auto-policy', component: AutoPolicyComponent },
-
+      
     ]
   },
-
+  { path: 'manage-policies', component: ManagePoliciesComponent },
 
   { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
-
-  {
-    path: 'admin/dashboard',
-    component: AdminDashboardComponent,
-  },
-  {
-    path: 'admin/users',
-    component: UserManagementComponent,
-  },
-
 
 
 
